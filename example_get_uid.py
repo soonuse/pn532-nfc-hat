26e744ebd087c996483489a7a2cd9f532b4a0388
soonuse <116975428@qq.com>
2019-05-16 01:54:36 +0000

    Add files

---
+++ example_get_uid.py
@@ -0,0 +1,38 @@
+"""
+This example shows connecting to the PN532 with I2C (requires clock
+stretching support), SPI, or UART. SPI is best, it uses the most pins but
+is the most reliable and universally supported.
+After initialization, try waving various 13.56MHz RFID cards over it!
+"""
+
+import RPi.GPIO as GPIO
+#from pn532.i2c import PN532_I2C
+#from pn532.spi import PN532_SPI
+from pn532.uart import PN532_UART
+
+
+if __name__ == '__main__':
+    try:
+        #pn532 = PN532_I2C(debug=False, reset=6, req=12)
+        #pn532 = PN532_SPI(debug=False, reset=6)
+        pn532 = PN532_UART(debug=False, reset=6)
+        
+        ic, ver, rev, support = pn532.get_firmware_version()
+        print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+        
+        # Configure PN532 to communicate with MiFare cards
+        pn532.SAM_configuration()
+        
+        print('Waiting for RFID/NFC card...')
+        while True:
+            # Check if a card is available to read
+            uid = pn532.read_passive_target(timeout=0.5)
+            print('.', end="")
+            # Try again if no card is available.
+            if uid is None:
+                continue
+            print('Found card with UID:', [hex(i) for i in uid])
+    except Exception as e:
+        print(e)
+    finally:
+        GPIO.cleanup()
