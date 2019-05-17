86f33899a021a17711087a2a866f86990cd0c493
soonuse <116975428@qq.com>
2019-05-17 06:21:01 +0000

    Add an example

---
+++ example_dump_mifare.py
@@ -0,0 +1,37 @@
+"""
+This example shows connecting to the PN532 and reading an M1
+type RFID tag
+"""
+
+import pn532.pn532 as nfc
+
+from pn532.spi import PN532_SPI
+#from pn532.uart import PN532_UART
+
+pn532 = PN532_SPI(debug=False)
+#pn532 = PN532_UART(debug=False)
+
+ic, ver, rev, support = pn532.get_firmware_version()
+print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+
+# Configure PN532 to communicate with MiFare cards
+pn532.SAM_configuration()
+
+print('Waiting for RFID/NFC card to read from!')
+while True:
+    # Check if a card is available to read
+    uid = pn532.read_passive_target(timeout=0.5)
+    print('.', end="")
+    # Try again if no card is available.
+    if uid is not None:
+        break
+print('Found card with UID:', [hex(i) for i in uid])
+
+key_a = b'\xFF\xFF\xFF\xFF\xFF\xFF'
+# Now we try to go through all 16 sectors (each having 4 blocks)
+for i in range(64):
+    if pn532.mifare_classic_authenticate_block(
+        uid, block_number=i, key_number=nfc.MIFARE_CMD_AUTH_A, key=key_a):
+        print(i, ':', 
+            ' '.join(['%02X' % x
+                for x in pn532.mifare_classic_read_block(i)]))
