d3dbd727bf86903ac7c3ddfe500ef77085adbaef
soonuse <116975428@qq.com>
2019-05-20 02:37:56 +0000

    Add descriptions

--- pn532/pn532.py
+++ pn532/pn532.py
@@ -1,3 +1,37 @@
+# Waveshare PN532 NFC Hat control library.
+# Author(s): Tony DiCola
+#			 refactor by Yehui from Waveshare
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015-2018 Adafruit Industries
+# Copyright (c) 2019 Waveshare
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+``pn532-nfc-hat``
+====================================================
+
+This module will let you communicate with a PN532 NFC Hat using I2C, SPI or UART.
+The main difference is the interfaces implements.
+"""
+
 import RPi.GPIO as GPIO
 
 __version__ = "0.0.0-auto.0"
@@ -107,8 +141,6 @@ _GPIO_VALIDATIONBIT            = 0x80
 _ACK                           = b'\x00\x00\xFF\x00\xFF\x00'
 _FRAME_START                   = b'\x00\x00\xFF'
 # pylint: enable=bad-whitespace
-
-
 
 
 class BusyError(Exception):
